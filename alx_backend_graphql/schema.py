2c619e424957a6f736e3adbe0d57201198604b8c
GO-Shoderu <go.shoderu@gmail.com>
2025-12-14 18:46:30 +0200

    Fixing checker errors

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,12 @@
 import graphene
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
+# Project-level Query that extends the CRM app Query
+class Query(CRMQuery, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+# Project-level Mutation that extends the CRM app Mutation
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
