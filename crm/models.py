67b71bab6c8943f793496fae5b59bd5e624fcc27
GO-Shoderu <go.shoderu@gmail.com>
2025-12-14 18:56:03 +0200

    Seeding, correction and Migration

--- crm/models.py
+++ crm/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
